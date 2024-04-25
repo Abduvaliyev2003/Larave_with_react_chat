08b913725ae539574392ee0e2b0739478ca48aca
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2024-04-25 18:37:01 +0500

    auth

---
+++ resources/js/Layouts/ChatLayout.jsx
@@ -0,0 +1,21 @@
+import { usePage } from "@inertiajs/react";
+
+
+
+ const  ChatLayout = ({children}) => {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const selectedConversation = page.props.selectedConversation;
+
+    return (
+        <>
+            ChatLayout
+            <div>
+                {children}
+            </div>
+        </>
+    );
+}
+
+
+export default ChatLayout;
