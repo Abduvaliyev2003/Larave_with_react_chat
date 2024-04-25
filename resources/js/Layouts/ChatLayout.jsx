cb4a694c1243c236cc355c0c2d8e2e53cad62d69
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2024-04-25 23:49:52 +0500

    change

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,4 +1,6 @@
 import { usePage } from "@inertiajs/react";
+import Echo from "laravel-echo";
+import { useEffect } from "react";
 
 
 
@@ -6,7 +8,21 @@ import { usePage } from "@inertiajs/react";
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
-
+    useEffect(() => {
+        Echo.join('online')
+            .here((users) => {
+                console.log('here', users)
+            })
+            .joining((user) => {
+                console.log('join', user)
+            })
+            .leaving((user) => {
+                console.log('join', user)
+            })
+            .error((error) => {
+                console.log('error' , error);
+            })
+    }, []);
     return (
         <>
             ChatLayout
