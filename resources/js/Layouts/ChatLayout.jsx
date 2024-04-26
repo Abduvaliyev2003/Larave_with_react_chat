194870ad1280c90749378bbd994f8909abfe87e5
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2024-04-26 14:55:17 +0500

    sidebar data

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,5 +1,6 @@
 import { usePage } from "@inertiajs/react";
 import Echo from "laravel-echo";
+import { useState } from "react";
 import { useEffect } from "react";
 
 
@@ -8,20 +9,69 @@ import { useEffect } from "react";
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
+    const [onlineUsers, setOnlineUsers] = useState({});
+
+    const isUserOnline = (userId) => onlineUsers[userId];
     useEffect(() => {
-        Echo.join('online')
+         setSortedConversations(
+            localConversations.sort((a,b) => {
+                if(a.blocked_at && b.blocked_at){
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                } else if (a.blocked_at){
+                    return 1;
+                } else if (b.blocked_at){
+                    return -1;
+                }
+                if(a.last_message_date && b.last_message_date){
+                    return b.last_message_date.localeCompare(
+                        a.last_message_date
+                    );
+                } else if(a.last_message_date){
+                    return -1;
+                } else if(b.last_message_date){
+                    return 1;
+                } else  {
+                    return 0;
+                }
+            })
+         );
+     }, [localConversations]);
+    useEffect(() => {
+       setLocalConversations(conversations)
+    }, [conversations]);
+    useEffect(() => {
+        window.Echo.join('online')
             .here((users) => {
-                console.log('here', users)
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                )
+
+                setOnlineUsers((prevOnlineUsers) => {
+                    return {...prevOnlineUsers, ...onlineUsersObj}
+                });
             })
             .joining((user) => {
-                console.log('join', user)
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updateUsers = { ...prevOnlineUsers};
+                    updateUsers[user.id] = user;
+                    return updateUsers;
+                });
             })
             .leaving((user) => {
-                console.log('join', user)
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updateUsers = { ...prevOnlineUsers};
+                    delete  updateUsers[user.id];
+                    return updateUsers;
+                });
             })
             .error((error) => {
                 console.log('error' , error);
             })
+        return () => {
+            window.Echo.leave('online');
+        }
     }, []);
     return (
         <>
